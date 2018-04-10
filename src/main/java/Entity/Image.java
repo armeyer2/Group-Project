ad7f315b022755e27500c0249cace70fdf01a29b
Aaron Goldbin <agoldbin@madisoncollege.edu>
2018-04-10 13:25:52 -0500

    imageSet instantiated

---
+++ src/main/java/Entity/Image.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Image {
+
+
+
+    public Set<String> imageSet = new HashSet<String>(Arrays.asList(msg, imgSrc));
+
+    public Set<String> getImageSet() {
+        return imageSet;
+    }
+}
